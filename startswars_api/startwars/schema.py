38ef540fd95fa48bb3eeee679b89895162ae23f0
daniel374 <dannilayton374@gmail.com>
2020-07-25 03:07:03 -0500

    ajustes y creación de mutations pelicula y personaje

--- startswars_api/startwars/schema.py
+++ startswars_api/startwars/schema.py
@@ -3,7 +3,7 @@ from graphene import relay, ObjectType, Field, InputObjectType
 from graphene_django.debug import DjangoDebug
 from graphene_django.filter import DjangoFilterConnectionField
 
-from .models import Planetas
+from .models import Planetas, Personajes, Peliculas
 
 from .nodes import PeliculasNode, PlanetasNode, PersonajesNode, PersopelisNode, PlanetaspelisNode
 
@@ -57,7 +57,63 @@ class CreatePlaneta(graphene.Mutation):
         planeta_instance.save()
         return CreatePlaneta(ok=ok, planeta=planeta_instance)
 
+""" 
+    Mutation personaje
+"""
+class PersonajeInput(graphene.InputObjectType):
+    idpj = graphene.ID()
+    nombrepj = graphene.String()
 
+class CreatePersonaje(graphene.Mutation):
+
+    class Arguments:
+        input = PersonajeInput(required=True)
+
+    ok = graphene.Boolean()
+    personaje = graphene.Field(PersonajesNode)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        ok = True
+        personaje_instance = Personajes(nombrepj=input.nombrepj)
+        personaje_instance.save()
+        return CreatePersonaje(ok=ok, personaje=personaje_instance)
+
+""" 
+    Mutation pelicula
+"""
+class PeliculaInput(graphene.InputObjectType):
+    idpl = graphene.ID()
+    nombrepl = graphene.String()
+    productorespl = graphene.String()
+    detallePl = graphene.String()
+    directorpl = graphene.String()
+
+class CreatePelicula(graphene.Mutation):
+
+    class Arguments:
+        input = PeliculaInput(required=True)
+
+    ok = graphene.Boolean()
+    pelicula = graphene.Field(PeliculasNode)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        ok = True
+        pelicula_instance = Peliculas(
+            nombrepl=input.nombrepl,
+            productorespl=input.productorespl,
+            detallePl=input.detallePl,
+            directorpl=input.directorpl
+            )
+        pelicula_instance.save()
+        return CreatePelicula(ok=ok, pelicula=pelicula_instance)
+
+"""
+    Register mutations
+"""
 class Mutation(ObjectType):
     create_planeta = CreatePlaneta.Field()
+    create_personaje = CreatePersonaje.Field()
+    create_pelicula = CreatePelicula.Field()
     
